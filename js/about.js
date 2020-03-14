fa123326cd3ab1f9254b6c5e7515b41948c69fd9
Yash Kadam <yashkadam278@gmail.com>
2020-03-14 16:48:27 +0530

    Inital Commit

---
+++ js/about.js
@@ -0,0 +1,55 @@
+const text = document.querySelector(".title");
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+
+for (const currentLetter of splitText) {
+  if (currentLetter === ",") {
+    text.innerHTML += `<span class="title-anim">${currentLetter}</span><br />`;
+  } else if (currentLetter === "h") {
+    text.innerHTML += `<span class="title-anim">${currentLetter}</span><br />`;
+  } else if (currentLetter === " ") {
+    text.innerHTML += `<span class="title-anim">&nbsp;</span>`;
+  } else {
+    text.innerHTML += `<span class="title-anim">${currentLetter}</span>`;
+  }
+}
+
+let char = 0;
+let timer = setInterval(onTick, 80);
+
+function onTick() {
+  const span = text.querySelectorAll(".title-anim")[char];
+  span.classList.add("rubberBand", "fade-in");
+  char++;
+  if (char === splitText.length) {
+    complete();
+    $(".title-anim").bind(
+      "webkitAnimationEnd mozAnimationEnd animationEnd",
+      function() {
+        $(this).removeClass("rubberBand");
+      }
+    );
+    // document.querySelector(".contact-btn").classList.add("fade-in");
+    // document.querySelector(".Animation").classList.add("fade-in");
+    // document.querySelector(".social").classList.add("fade-in");
+    document.querySelector(".subtitle").classList.add("fade-in");
+    return;
+  }
+}
+
+function complete() {
+  clearInterval(timer);
+  timer = null;
+}
+
+$(".title-anim").bind(
+  "webkitAnimationEnd mozAnimationEnd animationEnd",
+  function() {
+    $(this).removeClass("rubberBand");
+  }
+);
+
+$(".title-anim").hover(function() {
+  $(this).addClass("rubberBand");
+});
